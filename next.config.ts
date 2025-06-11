37c0504e02bfcf8b02cbde080f4a14483890760b
Marc PC <martinezmarc228@gmail.com>
2025-06-11 19:17:26 +0800

    Test

--- next.config.ts
+++ next.config.ts
@@ -7,22 +7,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
 
-  webpack: (config) => {
-    
-    config.resolve = {
-      ...config.resolve,
-      fallback: {
-        ...config.resolve?.fallback,
-        fs: false,
-        net: false,
-        dns: false,
-        child_process: false,
-        tls: false,
-      },
-    };
-
-    return config;
-  },
+  
 };
 
 export default nextConfig;
